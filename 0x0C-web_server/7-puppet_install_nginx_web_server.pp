a8bb56b8efa036d809b7b507a04e36a9b2a5173e
colinochieng <colinokumu89@gmail.com>
2023-07-03 22:04:54 +0300

    nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,35 +1,67 @@
-# configuring  server with Puppet
+#
 
-package {'nginx':
-  ensure => present,
+package { 'nginx':
+  ensure => installed,
 }
 
-exec {'installation':
-  command  => 'sudo apt-get update -y; sudo apt-get install -y nginx',
-  provider => shell,
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
 }
 
-exec {'firewall':
-  command  => 'sudo ufw allow "nginx http"',
-  provider => shell,
+exec { 'allow_nginx_http':
+  command => 'ufw allow "nginx http"',
+  unless => 'ufw status | grep -q "80"',
+  require => Package['nginx'],
 }
 
-exec {'Hello':
-  command  => 'echo "Hello world!" | sudo tee /var/www/html/index.nginx-debian.html > /dev/null',
-  provider => shell,
+file { '/var/www/html':
+  ensure => directory,
+  mode => '0755',
 }
 
-exec {'err404':
-  command  => 'Ceci n\'est pas une page" | sudo tee /var/www/html/custom_404.html > /dev/null',
-  provider => shell,
+file { '/var/www':
+  ensure => directory,
+  mode => '0755',
+  recurse => true,
 }
 
-exec {'configuration':
-  command  => 'sudo sed -i \'0,/location \/ {/ s|location / {|error_page 404 /custom_404.html;\n\n\tlocation  ~ ^/redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=zJ6WbK9zFpI;\n|\' /etc/nginx/sites-available/default',
-  provider => shell,
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello world!',
+  mode => '0644',
+  require => File['/var/www/html'],
 }
 
-exec {"restart":s
-  command => 'sudo service nginx restart',
-  provider => shell,
+file { '/var/www/html/custom_404.html':
+  content => "Ceci n'est pas une page",
+  mode => '0644',
+  require => File['/var/www/html'],
 }
+
+file { '/etc/nginx/sites-available/default':
+  content => @(END),
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+                rewrite ^/redirect_me(.*)\$ https://www.youtube.com/watch?v=70JD5YTemJc permanent;
+        }
+
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+                internal;
+        }
+}
+END
+  require => Package['nginx'],
+  notify => Service['nginx'],
+}
+
