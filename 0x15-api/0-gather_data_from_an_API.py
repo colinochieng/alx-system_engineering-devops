47ed01019ba972f93026976ab297e0817abeb5a2
colinochieng <colinokumu89@gmail.com>
2023-07-24 21:52:58 +0300

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,10 @@
 Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-from urllib.request import urlopen
-from sys import argv, exit
 import json
+from sys import argv, exit
+from urllib.request import urlopen
+
 
 
 URL = 'https://jsonplaceholder.typicode.com'
@@ -18,8 +19,9 @@ if __name__ == '__main__':
         exit(1)
     user_data = json.loads(urlopen(f'{URL}/users/{id}').read().decode('utf-8'))
     todos = json.loads(urlopen(f'{URL}/todos').read().decode('utf-8'))
-    todo_list = list(filter(lambda dictionary: dictionary.get('userId') == id, todos))
-    task_done = list(filter(lambda dic: dic.get('completed') == True, todo_list))
-    print(f'Employee {user_data.get("name")} is done with tasks({len(task_done)}/{len(todo_list)}):')
+    todo_list = list(filter(lambda dic: dic.get('userId') == id, todos))
+    task_done = list(filter(lambda dic: dic.get('completed'), todo_list))
+    print(f'''Employee {user_data.get("name")}
+          is done with tasks({len(task_done)}/{len(todo_list)}):''')
     for dic in task_done:
         print('\t', dic.get('title'))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+
+"""
+import urllib.parse
+
+print(urllib.request.pathname2url('https://docs.python.org/3/library/urllib.request.html#module-urllib.request'))
