9b1112329da75ff9f67d90a93c3bbcea1aeee42c
colinochieng <colinokumu89@gmail.com>
2023-07-24 21:01:38 +0300

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+from urllib.request import urlopen
+from sys import argv
+import json
+
+
+URL = 'https://jsonplaceholder.typicode.com'
+
+
+if __name__ == '__main__':
+    try:
+        id = int(argv[1])
+    except ValueError as e:
+        pass
+    user_data = json.loads(urlopen(f'{URL}/users/{id}').read().decode('utf-8'))
+    todos = json.loads(urlopen(f'{URL}/todos').read().decode('utf-8'))
+    todo_list = list(filter(lambda dictionary: dictionary.get('userId') == id, todos))
+    task_done = list(filter(lambda dic: dic.get('completed') == True, todo_list))
+    print(f'Employee {user_data.get("name")} is done with tasks({len(task_done)}/{len(todo_list)}):')
+    for dic in task_done:
+        print('\t ',  dic.get('title'))
