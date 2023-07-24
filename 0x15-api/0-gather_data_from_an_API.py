569bf4f2398e5b2f942725d82c4d7fb99d65e7f9
colinochieng <colinokumu89@gmail.com>
2023-07-24 12:07:20 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,9 @@
 Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-from urllib.request import urlopen
-from sys import argv, exit
 import json
+import sys
+from urllib.request import urlopen
 
 
 URL = 'https://jsonplaceholder.typicode.com'
@@ -13,13 +13,15 @@ URL = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
     try:
-        id = int(argv[1])
+        id = int(sys.argv[1])
     except ValueError:
-        exit(1)
+        sys.exit(1)
     user_data = json.loads(urlopen(f'{URL}/users/{id}').read().decode('utf-8'))
     todos = json.loads(urlopen(f'{URL}/todos').read().decode('utf-8'))
-    todo_list = list(filter(lambda dictionary: dictionary.get('userId') == id, todos))
-    task_done = list(filter(lambda dic: dic.get('completed') == True, todo_list))
-    print(f'Employee {user_data.get("name")} is done with tasks({len(task_done)}/{len(todo_list)}):')
+    todo_list = list(filter(lambda dic: dic.get('userId') == id, todos))
+    task_done = list(filter(lambda dic: dic.get('completed'), todo_list))
+    name = user_data.get("name")
+    ratio = [len(task_done), len(todo_list)]
+    print(f'Employee {name} is done with tasks({ratio[0]}/{ratio[1]}):')
     for dic in task_done:
         print('\t', dic.get('title'))
