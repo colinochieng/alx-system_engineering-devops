9589bfd93e91f97ce3b6f8c7124753af3cc43b20
colinochieng <colinokumu89@gmail.com>
2023-07-24 22:59:56 +0300

    API csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
 """
-
+Script that, using this REST API, for a given employee ID
+Records all tasks that are owned by this employee in csv file
 """
-import urllib.parse
+import csv
+import json
+import sys
+from urllib.request import urlopen
 
-print(urllib.request.pathname2url('https://docs.python.org/3/library/urllib.request.html#module-urllib.request'))
+
+URL = 'https://jsonplaceholder.typicode.com'
+
+
+if __name__ == '__main__':
+    try:
+        id = int(sys.argv[1])
+    except ValueError:
+        sys.exit(1)
+    user_data = json.loads(urlopen(f'{URL}/users/{id}').read().decode('utf-8'))
+    todos = json.loads(urlopen(f'{URL}/todos').read().decode('utf-8'))
+    todo_list = list(filter(lambda dic: dic.get('userId') == id, todos))
+    name = user_data.get("username")
+
+    def delete_keys(dic):
+        """removes keys from a dictionary"""
+        new = {}
+        new.update({'USER_ID': dic.get('userId')})
+        new.update({'USERNAME': name})
+        new.update({'TASK_COMPLETED_STATUS': dic.get('completed')})
+        new.update({'TASK_TITLE': dic.get('title')})
+
+        return new
+    
+    
+    alter_list = list(map(delete_keys, todo_list))
+    
+    field_names = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+
+    with open(f'{id}.csv', 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=field_names, quoting=csv.QUOTE_ALL)
+        writer.writerows(alter_list)
+    
