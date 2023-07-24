41309448c25b8e50ce689757a43a63713d8b76cf
colinochieng <colinokumu89@gmail.com>
2023-07-24 21:55:14 +0300

    hbnb

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,10 +3,9 @@
 Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import json
-from sys import argv, exit
 from urllib.request import urlopen
-
+from sys import argv, exit
+import json
 
 
 URL = 'https://jsonplaceholder.typicode.com'
@@ -19,9 +18,8 @@ if __name__ == '__main__':
         exit(1)
     user_data = json.loads(urlopen(f'{URL}/users/{id}').read().decode('utf-8'))
     todos = json.loads(urlopen(f'{URL}/todos').read().decode('utf-8'))
-    todo_list = list(filter(lambda dic: dic.get('userId') == id, todos))
-    task_done = list(filter(lambda dic: dic.get('completed'), todo_list))
-    print(f'''Employee {user_data.get("name")}
-          is done with tasks({len(task_done)}/{len(todo_list)}):''')
+    todo_list = list(filter(lambda dictionary: dictionary.get('userId') == id, todos))
+    task_done = list(filter(lambda dic: dic.get('completed') == True, todo_list))
+    print(f'Employee {user_data.get("name")} is done with tasks({len(task_done)}/{len(todo_list)}):')
     for dic in task_done:
         print('\t', dic.get('title'))
