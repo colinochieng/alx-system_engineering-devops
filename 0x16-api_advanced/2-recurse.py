a241fff9f626ec0562fa69b0f9d8e7047d7638ba
colinochieng <colinokumu89@gmail.com>
2023-08-10 22:24:40 +0300

    start

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ Module for recursive function that queries the Reddit API
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """
 
     :param subreddit: subreddit
@@ -13,4 +13,26 @@ def recurse(subreddit, hot_list=[]):
     :return: a list containing the titles of all
     hot articles for a given subreddit
     """
-    return []
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'Content-Type': 'application/json',
+               'User-Agent': 'MyApp/1.0 (colinokumu89@gmail.com)'
+               }
+    limit = 100
+    params = {
+        'limit': limit,
+        "count": count
+    }
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.status_code:
+        data = response.json().get('data')
+        after = data.get("after")
+        count = data.get("dist")
+        records = data.get('children')
+        for i in records:
+            hot_list.append(i.get('data').get('title'))
+        if after:
+            return recurse(subreddit, hot_list, after=after, count=count)
+        return hot_list
+    else:
+        return None
